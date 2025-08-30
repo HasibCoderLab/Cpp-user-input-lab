5fba58db9e462e1877b5d30f579fa821de9f4d88
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-30 17:39:21 +0600

    update 6th example of 'User-Input' in C++

--- example06.cpp
+++ example06.cpp
@@ -16,3 +16,19 @@ int main(){
 }
 
 // ====================   Best Way ===============
+#include <iostream>
+using namespace std;
+int main(){
+    float x;
+    float y;
+  cout<< "Enter 1st Number : ";
+  cin>> x;
+  cout <<"Enter 2nd Number : ";
+  cin>> y;
+  if(y==0){
+    cout<<"Error: Division by zero is not allowed!" << endl;
+  }else{
+    cout << "Result of division: " << x / y << endl;
+  }
+    return 0;
+}
