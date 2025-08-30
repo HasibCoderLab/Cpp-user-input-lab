877370b504c539b3c20b58ed3cfcf8836f714a92
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-30 17:15:59 +0600

    add 2nd example

---
+++ example02.cpp
@@ -0,0 +1,15 @@
+
+// ================================================
+//               Language: C++ / Cpp
+//               Topic: User Input  || 👉👉  Example   02
+// ================================================
+#include <iostream>
+using namespace std;
+int main(){
+  int x, y ;
+  cout<<"Enter 2 Number : ";
+  cin>> x>> y;
+  cout<<"Sum is " <<x+y;
+  cout<<endl;
+    return 0;
+}
