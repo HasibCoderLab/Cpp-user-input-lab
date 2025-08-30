e5daffc30074ddf97355126e7d72b4b9ac6a0210
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-30 15:43:34 +0600

    add 1st example

---
+++ input.cpp
@@ -0,0 +1,17 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic: User Input
+// ================================================
+
+
+
+
+#include  <iostream>
+using namespace std;
+int main (){
+int x , y;
+cout<< "Enter Number : ";
+cin>> x;
+cout <<"You Entered : " <<x << endl;
+    return 0;
+}
