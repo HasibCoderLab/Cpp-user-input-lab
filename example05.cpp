1d4d49dd0cfa25e6063b68f6d59e8094d7053ea7
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-30 17:26:48 +0600

    add 5th example of 'User-Input' in C++

---
+++ example05.cpp
@@ -0,0 +1,16 @@
+// ================================================
+//               Language: C++ / Cpp
+//               Topic   : [User-Input]
+//               Example : 👉👉 [5]
+// ================================================
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    int y;
+    cout << "Enter 1st Number : ";
+    cin>> x;
+    cout<<"Enter 2nd Number : ";
+    cin>> y;
+    cout<<"Sum is " << x-y <<endl;
+}
